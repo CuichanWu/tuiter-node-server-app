365fd3bc8ac8d3542fb26251ced50d99e3a92c8c
CuichanWu <wu.cu@northeastern.edu>
2023-03-30 09:23:25 -0700

    updated code but with broken new tuit code

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import HelloController from "./controllers/hello-controller.js";
 import UserController from "./controllers/users/users-controller.js";
-import TuitController from "./controllers/tuits/tuits-controller.js";
+import TuitsController from "./controllers/tuits/tuits-controller.js";
+// import ProfileController from "./controllers/profile/profile-controller.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-TuitController(app);
+TuitsController(app);
 HelloController(app);
 UserController(app);
+// ProfileController(app);
 app.listen(process.env.PORT || 4000);
