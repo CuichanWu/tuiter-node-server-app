582c5c8931e9963b639c3842e9b49b130ba30ed0
CuichanWu <wu.cu@northeastern.edu>
2023-04-01 15:52:57 -0700

    a9 branch created

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 import HelloController from "./controllers/hello-controller.js";
 import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
+import mongoose from "mongoose";
 // import ProfileController from "./controllers/profile/profile-controller.js";
 
+mongoose.connect("mongodb://127.0.0.1:27017/tuiter");
 const app = express();
 app.use(cors());
 app.use(express.json());
