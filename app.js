7049b1d93ffbf9115a2ed65b4ed72597cc692682
CuichanWu <wu.cu@northeastern.edu>
2023-04-04 00:37:50 -0700

    a9 Mongo

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ import TuitsController from "./controllers/tuits/tuits-controller.js";
 import mongoose from "mongoose";
 // import ProfileController from "./controllers/profile/profile-controller.js";
 
-mongoose.connect("mongodb://127.0.0.1:27017/tuiter");
+const CONNECTION_STRING =
+  process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/tuiter";
+mongoose.connect(CONNECTION_STRING);
+// mongoose.connect("mongodb://127.0.0.1:27017/tuiter");
 const app = express();
 app.use(cors());
 app.use(express.json());
--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -1,9 +1,23 @@
 import mongoose from "mongoose";
 const schema = mongoose.Schema(
   {
+    topic: String,
+    username: String,
+    handle: String,
+    time: String,
+    image: String,
+    title: String,
     tuit: String,
-    likes: Number,
     liked: Boolean,
+    likes: Number,
+    dislikes: Number,
+    disliked: Boolean,
+    replies: Number,
+    retuits: Number,
+
+    // tuit: String,
+    // likes: Number,
+    // liked: Boolean,
   },
   { collection: "tuits" }
 );
